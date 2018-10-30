0db2a26473188dc735fc6e3ce6463294a7dcebe6
kumarinie <kumarisushmanie@gmail.com>
2018-10-30 14:31:00 +0530

    Task: T0014 - Added manifest in commit Repo: kumarinie/magnus-addons Module: magnus_invoicing

---
+++ magnus_invoicing/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Willem Hulshof Magnus (www.magnus.nl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': "magnus_invoicing",
+
+    'summary': """
+        This module introduces an advanced professional services invoicing process,
+        offering fixed price, time and material, licensing and several combinations
+         thereof """,
+
+    'description': """
+        This module introduces an advanced professional services invoicing process,
+        offering fixed price, time and material, licensing and several combinations
+         thereof. 
+    """,
+
+    'author': "Magnus",
+    'website': "http://www.magnus.nl",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'module_category_specific_industry_applications',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['date_range','hr_timesheet_sheet','account_fiscal_month','account_fiscal_year'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'data/data.xml',
+        'wizard/analytic_line_invoice_view.xml',
+        'views/hr_view.xml',
+        'views/task_view.xml',
+        'views/account_analytic_view.xml',
+        'views/templates.xml',
+        'views/analytic_invoice.xml',
+        'views/menuitem.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+#        'demo/demo.xml',
+    ],
+}
