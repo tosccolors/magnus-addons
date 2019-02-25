c3ff6d1f4e8b4373fb45dae2ffcc2b30bdd2ebe5
Willem Hulshof <w.hulshof@magnus.nl>
2019-02-25 10:21:16 +0100

    experiments with partner_multi_relation

--- magnus_partner_multi_relation/models/res_partner_relation_all.py
+++ magnus_partner_multi_relation/models/res_partner_relation_all.py
@@ -6,36 +6,6 @@ import logging
 import collections
 from odoo import _, api, fields, models
 
-# Register relations
-RELATIONS_SQL_M = """\
-SELECT
-    (rel.id * %%(padding)s) + %(key_offset)s AS id,
-    'res.partner.relation' AS res_model,
-    rel.id AS res_id,
-    rel.left_partner_id AS this_partner_id,
-    rel.right_partner_id AS other_partner_id,
-    rel.type_id,
-    rel.date_start,
-    rel.date_end,
-    rel.distribution_key,
-    %(is_inverse)s as is_inverse
-FROM res_partner_relation rel"""
-
-# Register inverse relations
-RELATIONS_SQL_INVERSE_M = """\
-SELECT
-    (rel.id * %%(padding)s) + %(key_offset)s AS id,
-    'res.partner.relation',
-    rel.id,
-    rel.right_partner_id,
-    rel.left_partner_id,
-    rel.type_id,
-    rel.date_start,
-    rel.date_end,
-    rel.distribution_key,
-    %(is_inverse)s as is_inverse
-FROM res_partner_relation rel"""
-
 
 class ResPartnerRelationAll(models.AbstractModel):
     """Abstract model to show each relation from two sides."""
@@ -45,11 +15,17 @@ class ResPartnerRelationAll(models.AbstractModel):
         string='Percentage Distribution Key'
     )
 
-    def get_register(self):
-        register = collections.OrderedDict()
-        register['_lastkey'] = -1
-        self.register_specification(
-            register, 'relation', False, RELATIONS_SQL_M)
-        self.register_specification(
-            register, 'relation', True, RELATIONS_SQL_INVERSE_M)
-        return register
+
+    def _get_additional_view_fields(self):
+        """Add distribution_key to view fields."""
+        return ','.join([
+            super(ResPartnerRelationAll, self)._get_additional_view_fields(),
+            " ext.distribution_key as distribution_key"])
+
+    def _get_additional_tables(self):
+        """Add res_partner_tab table to view."""
+        # pylint: disable=no-member
+        return ' '.join([
+            super(ResPartnerRelationAll, self)._get_additional_tables(),
+            "JOIN res_partner_relation_extension ext"
+            " ON bas.id = ext.relation_id"])
