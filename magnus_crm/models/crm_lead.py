9f207a779c66b2583680d0b9af8fa8adb671c623
praveen-kumarG <praveenkg@systemsvalley.co.in>
2019-04-08 19:07:46 +0530

    T0035 - Additions in Monthly Revenue overview Module: magnus_crm.

--- magnus_crm/models/crm_lead.py
+++ magnus_crm/models/crm_lead.py
@@ -72,7 +72,7 @@ class Lead(models.Model):
                 weighted_revenue_per_month = (((float(days_per_month)/float(total_days))*self.planned_revenue)*(self.probability/100))
                 days = " days (" if days_per_month > 1 else " day ("
                 duration = str(days_per_month) + days + str(sd.day)+"-"+str(month_end_date.day)+" "+str(sd.strftime('%B'))+")"
-                monthly_revenues.append((0, 0, {'date': month_end_date, 'latest_revenue_date': month_end_date.replace(day=1) - timedelta(days=1), 'month': month_end_date.strftime('%B'),'no_of_days': duration,'weighted_revenue': weighted_revenue_per_month, 'expected_revenue': expected_revenue_per_month,'percentage': self.probability, 'computed_line': True, 'percentage': self.probability}))
+                monthly_revenues.append((0, 0, {'date': month_end_date, 'latest_revenue_date': month_end_date.replace(day=1) - timedelta(days=1), 'year': month_end_date.year, 'month': month_end_date.strftime('%B'),'no_of_days': duration,'weighted_revenue': weighted_revenue_per_month, 'expected_revenue': expected_revenue_per_month,'percentage': self.probability, 'computed_line': True, 'percentage': self.probability}))
                 sd = month_end_date + timedelta(days=1)
                 month_end_date = (sd + relativedelta(months=1)).replace(day=1) - timedelta(days=1)
                 if sd > ed:
@@ -165,17 +165,22 @@ class MonthlyRevenue(models.Model):
                 res['latest_revenue_date'] = latest_revenue_date.strftime("%Y-%m-%d")
         return res
 
-    month = fields.Char('Month', required=True)
     date = fields.Date('Date', required=True)
+    year = fields.Char(string='Year')
+    month = fields.Char(string='Month')
+    no_of_days = fields.Char(string='Duration')
     latest_revenue_date = fields.Date('Latest Revenue Date')
-    no_of_days = fields.Char('Duration', required=True)
     weighted_revenue = fields.Float('Weighted Revenue', required=True)
     expected_revenue = fields.Float('Expected Revenue', required=True)
     percentage = fields.Float(string='Probability')
-    lead_id = fields.Many2one('crm.lead', string='Lead', ondelete='cascade', required=True)
+    lead_id = fields.Many2one('crm.lead', string='Opportunity', ondelete='cascade', required=True)
     company_currency = fields.Many2one(string='Currency', related='lead_id.company_id.currency_id', readonly=True, relation="res.currency", store=True)
     user_id = fields.Many2one(related="lead_id.user_id", relation='res.users', string='Salesperson', index=True, store=True)
     computed_line = fields.Boolean(string="Computed line")
+    project_id = fields.Many2one('project.project', related='lead_id.project_id', string='Project', store=True)
+    partner_id = fields.Many2one('res.partner', related='lead_id.partner_id', string='Customer', store=True)
+    sector_id = fields.Many2one('res.partner.sector', related='lead_id.sector_id', string='Main Sector', store=True)
+    department_id = fields.Many2one('hr.department', related='lead_id.department_id', string='Sales Team', store=True)
 
     @api.onchange('expected_revenue', 'percentage')
     def onchagne_expected_revenue(self):
@@ -199,6 +204,7 @@ class MonthlyRevenue(models.Model):
             days = " days (" if date.day > 1 else " day ("
             self.no_of_days = str(date.day)+days+str(1)+"-"+str(date.day)+" "+str(date.strftime('%B'))+")"
             self.month = date.strftime('%B')
+            self.year = date.year
 
         if lead_id and date:
             lead = self.env['crm.lead'].browse([lead_id])
