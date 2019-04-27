541193a0612b7ddbe1925e2dbbe0039efcd610a4
Willem Hulshof <w.hulshof@magnus.nl>
2019-04-27 17:26:47 +0200

    pretty good chargeability report

--- magnus_timesheet/report/hr_chargeability_report.py
+++ magnus_timesheet/report/hr_chargeability_report.py
@@ -9,12 +9,12 @@ class HrChargeabilityReport(models.Model):
 
     date = fields.Date('Date', readonly=True)
     user_id = fields.Many2one('res.users', string='User', readonly=True)
-    project_operating_unit_id = fields.Many2one('operating.unit', string='Operating Unit', readonly=True)
-    chargeable_hours = fields.Float(string="Charegeable Hrs", readonly=True)
+    captured_hours = fields.Float(string="Captured Hrs", readonly=True)
+    chargeable_hours = fields.Float(string="Chargeable Hrs", readonly=True)
     norm_hours = fields.Float(string="Norm Hrs", readonly=True)
     chargeability = fields.Float(string="Chargeability", readonly=True)
     department_id = fields.Many2one('hr.department', string='Department', readonly=True)
-    dept_operating_unit_id = fields.Many2one('operating.unit', string='Dept. Operating Unit', readonly=True)
+    operating_unit_id = fields.Many2one('operating.unit', string='Department Operating Unit', readonly=True)
     external = fields.Boolean(string='External', readonly=True)
 
 
@@ -29,10 +29,10 @@ class HrChargeabilityReport(models.Model):
                     min(aa.id) as id,
                     aa.date as date,
                     aa.user_id as user_id,
-                    aa.operating_unit_id as dept_operating_unit_id,
+                    aa.operating_unit_id as operating_unit_id,
                     aa.department_id as department_id,
-                    aa.project_operating_unit_id as project_operating_unit_id,
                     emp.external as external,
+                    SUM(unit_amount) as captured_hours,
                     SUM(CASE 
                              WHEN aa.chargeable = 'true' 
                              THEN unit_amount 
@@ -82,7 +82,7 @@ class HrChargeabilityReport(models.Model):
                                             WHEN aa.correction_charge = 'true' 
                                             THEN unit_amount 
                                             ELSE 0 
-                                    END)) * 100
+                                    END))
                     END	as chargeability
                 FROM
                     account_analytic_line aa
@@ -92,9 +92,8 @@ class HrChargeabilityReport(models.Model):
                 WHERE aa.product_uom_id = %s 
                     AND aa.planned = FALSE 
                     AND aa.project_id IS NOT NULL 
-                    AND (aa.correction_charge = true OR aa.chargeable = true)
                 GROUP BY aa.operating_unit_id, aa.user_id, dr.date_end, aa.date, aa.department_id, 
-                aa.project_operating_unit_id, emp.external
+                emp.external
 				ORDER BY aa.date
             )""" % (uom))
 
@@ -103,49 +102,50 @@ class HrChargeabilityReport(models.Model):
     def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
         res = super(HrChargeabilityReport, self).read_group(domain, fields, groupby, offset, limit=limit, orderby=orderby, lazy=lazy)
         for index, val in enumerate(res):
-            domain = val.get('__domain', False)
-
-            if domain:
-                where_query = self._where_calc(domain)
-                from_clause, where_clause, where_clause_params = where_query.get_sql()
-
-                list_query = ("""
-                        SELECT
-                            chargeable_hours, norm_hours
-                          FROM
-                            {0}
-                          WHERE {1}
-                     """.format(
-                    from_clause,
-                    where_clause
-                ))
-
-                self.env.cr.execute(list_query, where_clause_params)
-            else:
-                list_query = ("""
-                        SELECT
-                            chargeable_hours, norm_hours
-                          FROM
-                            hr_chargeability_report
-                     """
-                )
-                self.env.cr.execute(list_query)
-            result = self._cr.fetchall()
-            chargeable_hours, norm_hours = 0, 0
-            for r in result:
-                chargeable_hours += r[0]
-                norm_hours += r[1]
-            if chargeable_hours or norm_hours:
-                if norm_hours:
-                    if 'date:month' in groupby:
-                        norm_hours = (len(result)*8) - norm_hours
-                    # elif 'date:week' in groupby:
-                    #     norm_hours = (len(result)*8) - norm_hours
-                    elif 'user_id' in groupby and len(groupby) ==1 or not groupby:
-                        norm_hours = (len(result) * 8) - norm_hours
-                else:
-                    norm_hours = chargeable_hours #if norm_hrs zero chargeability would be 100%
-                norm_hours = norm_hours if norm_hours else 1
-                res[index]['chargeability'] = (chargeable_hours / norm_hours) * 100
+            # domain = val.get('__domain', False)
+            #
+            # if domain:
+            #     where_query = self._where_calc(domain)
+            #     from_clause, where_clause, where_clause_params = where_query.get_sql()
+            #
+            #     list_query = ("""
+            #             SELECT
+            #                 chargeable_hours, norm_hours
+            #               FROM
+            #                 {0}
+            #               WHERE {1}
+            #          """.format(
+            #         from_clause,
+            #         where_clause
+            #     ))
+            #
+            #     self.env.cr.execute(list_query, where_clause_params)
+            # else:
+            #     list_query = ("""
+            #             SELECT
+            #                 chargeable_hours, norm_hours
+            #               FROM
+            #                 hr_chargeability_report
+            #          """
+            #     )
+            #     self.env.cr.execute(list_query)
+            # result = self._cr.fetchall()
+            # chargeable_hours, norm_hours = 0, 0
+            # for r in result:
+            #     chargeable_hours += r[0]
+            #     norm_hours += r[1]
+            # if chargeable_hours or norm_hours:
+            #     if norm_hours:
+            #         if 'date:month' in groupby:
+            #             norm_hours = (len(result)*8) - norm_hours
+            #         # elif 'date:week' in groupby:
+            #         #     norm_hours = (len(result)*8) - norm_hours
+            #         elif 'user_id' in groupby and len(groupby) ==1 or not groupby:
+            #             norm_hours = (len(result) * 8) - norm_hours
+            #     else:
+            #         norm_hours = chargeable_hours #if norm_hrs zero chargeability would be 100%
+            #     norm_hours = norm_hours if norm_hours else 1
+            res[index]['chargeability'] = (res[index]['chargeable_hours'] / res[index]['norm_hours']) * 100 if res[
+                index]['norm_hours'] > 0 else 0.0
 
         return res
