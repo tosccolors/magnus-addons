7c5a323f62224c4fd332899f762e4b80c13367f3
kumarinie <kumarisushmanie@gmail.com>
2019-07-12 18:14:27 +0530

    Task: T0336 - Magnus Employee Landing Page Module: magnus_landing_page

---
+++ magnus_landing_page/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "magnus_landing_page",
+
+    'summary': """
+        Employee Landing Page""",
+
+    'description': """
+        Employee Landing Page
+    """,
+
+    'author': "Magnus",
+    'website': "http://www.magnus.nl",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Report',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['magnus_timesheet', 'magnus_holidays'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hr_employee_landing_page_views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+}
