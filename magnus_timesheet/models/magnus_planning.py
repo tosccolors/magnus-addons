bb01e2e15c2477719bc0da6a96f923ab97ed1519
kumarinie <kumarisushmanie@gmail.com>
2019-08-06 16:34:13 +0530

    T0345 : Optimized views module: magnus_timesheet

--- magnus_timesheet/models/magnus_planning.py
+++ magnus_timesheet/models/magnus_planning.py
@@ -17,6 +17,15 @@ class MagnusPlanning(models.Model):
     _description = "Planning"
     _rec_name = "user_id"
 
+    @api.one
+    @api.constrains('week_from', 'week_to')
+    def _check_weeks(self):
+        start_date = self.week_from.date_start
+        end_date = self.week_to.date_start
+        if (start_date and end_date) and (start_date > end_date):
+            raise ValidationError(_("End week should be greater than start week."))
+
+
     def _default_date_from(self):
         user = self.env['res.users'].browse(self.env.uid)
         r = user.company_id and user.company_id.timesheet_range or 'month'
@@ -43,41 +52,39 @@ class MagnusPlanning(models.Model):
         emp_ids = self.env['hr.employee'].search([('user_id', '=', self.env.uid)])
         return emp_ids and emp_ids[0] or False
 
-    name = fields.Char(string="Note", states={'confirm': [('readonly', True)], 'done': [('readonly', True)]})
+    name = fields.Char(string="Note")
     employee_id = fields.Many2one('hr.employee', string='Employee', default=_default_employee, required=True)
     user_id = fields.Many2one('res.users', related='employee_id.user_id', string='User', store=True, readonly=True)
-    date_from = fields.Date(string='Date From', default=_default_date_from, required=True,
-        index=True, readonly=True, states={'new': [('readonly', False)]})
-    date_to = fields.Date(string='Date To', default=_default_date_to, required=True,
-        index=True, readonly=True, states={'new': [('readonly', False)]})
-    planning_ids = fields.One2many('account.analytic.line', 'planning_id',
-        string='Planning lines',
-        readonly=True, states={
-            'draft': [('readonly', False)],
-            'new': [('readonly', False)]})
-    # state is created in 'new', automatically goes to 'draft' when created. Then 'new' is never used again ...
-    # (=> 'new' is completely useless)
-    state = fields.Selection([
-        ('new', 'New'),
-        ('draft', 'Open'),
-        ('confirm', 'Waiting Approval'),
-        ('done', 'Approved')], default='new', track_visibility='onchange',
-        string='Status', required=True, readonly=True, index=True,
-        help=' * The \'Open\' status is used when a user is encoding a new and unconfirmed timesheet. '
-             '\n* The \'Waiting Approval\' status is used to confirm the timesheet by user. '
-             '\n* The \'Approved\' status is used when the users timesheet is accepted by his/her senior.')
-    # account_ids = fields.One2many('hr_timesheet_sheet.sheet.account', 'sheet_id', string='Analytic accounts', readonly=True)
+    date_from = fields.Date(string='Date From', default=_default_date_from, required=True, index=True)
+    date_to = fields.Date(string='Date To', default=_default_date_to, required=True, index=True)
+    planning_ids = fields.One2many('account.analytic.line', 'planning_id', string='Planning lines')
     company_id = fields.Many2one('res.company', string='Company')
     department_id = fields.Many2one('hr.department', string='Department',
         default=lambda self: self.env['res.company']._company_default_get())
-
-    week_from = fields.Many2one('date.range', string='week from')
-    week_to = fields.Many2one('date.range', string='week to')
+    week_from = fields.Many2one('date.range', string='week from', required=True,index=True)
+    week_to = fields.Many2one('date.range', string='week to', required=True,index=True)
 
     @api.onchange('week_from', 'week_to')
     def onchange_week(self):
         self.date_from = self.week_from.date_start
         self.date_to = self.week_to.date_end
+
+    def unlink_analytic_entries(self):
+        analytic = self.planning_ids.filtered(lambda x: x.unit_amount == 0)
+        analytic.unlink()
+        return True
+
+    @api.model
+    def create(self ,vals):
+        res = super(MagnusPlanning, self).create(vals)
+        self.unlink_analytic_entries()
+        return res
+
+    @api.multi
+    def write(self, vals):
+        res = super(MagnusPlanning, self).write(vals)
+        self.unlink_analytic_entries()
+        return res
 
 
 class AccountAnalyticLine(models.Model):
