c01339cd198664072eea728f9754ae58d4ef4fdf
Willem Hulshof <w.hulshof@magnus.nl>
2019-12-08 21:22:07 +0100

    NOT RUNNING YET!!!!

--- magnus_invoicing/models/account_move.py
+++ magnus_invoicing/models/account_move.py
@@ -6,7 +6,7 @@
 from odoo.tools.translate import _
 from odoo import api, fields, models
 from odoo.exceptions import UserError
-
+from datetime import datetime, timedelta
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
@@ -52,5 +52,44 @@ class AccountMove(models.Model):
                     line.with_context(wip=True).unlink()
         res = super(AccountMove, self).post()
         return res
+
+    @api.multi
+    def wip_move_create(self, wip_journal):
+        self.ensure_one()
+        move_date = datetime.strptime(self.date, "%Y-%m-%d")
+        last_day_month_before = (move_date - timedelta(days=move_date.day)).strftime("%Y-%m-%d")
+        default = {
+            'ref': 'WIP Invoicing Posting',
+            'journal_id': wip_journal.id,
+            'date': last_day_month_before,
+            'narration': 'WIP Invoicing Posting',
+         #   'name': "new_name",
+            'to_be_reversed': True,
+        }
+        wip_move = self.copy(default)
+        mls = wip_move.line_ids
+        ids = []
+        ids.append(self.env.ref('account.data_account_type_other_income').id)
+        ids.append(self.env.ref('account.account.data_account_type_revenue').id)
+        ids.append(self.env.ref('account.account.data_account_type_depreciation').id)
+        ids.append(self.env.ref('account.account.data_account_type_expenses').id)
+        ids.append(self.env.ref('account.account.data_account_type_direct_costs').id)
+        bs_move_lines = mls.filtered(lambda r: r.account_id.type.id not in ids)
+        pl_move_lines = mls - bs_move_lines
+        amount = 0.0
+        for line in pl_move_lines:
+            amount += line.debit - line.credit
+        bs_move_lines.unlink()
+        ml_vals = {
+            'type': 'dest',
+            'name': name,
+            'price': total,
+            'account_id': self.env['account.invoice'].get_wip_default_account(),
+            'date_maturity': inv.date_due,
+            'amount_currency': diff_currency and total_currency,
+            'currency_id': diff_currency and inv.currency_id.id,
+        }
+        (0, 0, {values})
+
 
 
