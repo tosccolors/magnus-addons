e4ad55b0ef69268414730185751c6a2370527c52
Willem Hulshof <w.hulshof@magnus.nl>
2019-12-12 22:49:25 +0100

    credit and debit.....

--- magnus_invoicing/models/account_move.py
+++ magnus_invoicing/models/account_move.py
@@ -86,8 +86,8 @@ class AccountMove(models.Model):
         ar_line = mls.filtered(lambda r: r.account_id.id == ar_account_id)
         amount -= ar_line.debit - ar_line.credit
         bs_move_lines.unlink()
-        ar_line.credit = -amount if amount < 0 else 0
-        ar_line.debit = amount if amount > 0 else 0
+        ar_line.credit = amount if amount > 0 else 0
+        ar_line.debit = -amount if amount < 0 else 0
         ar_line.account_id = wip_journal.default_credit_account_id.id
         return wip_move
 
