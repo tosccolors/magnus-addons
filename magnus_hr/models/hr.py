2be3551e269136df905ef9fb867c082e4f7cdcf1
DeekshithSystemsValley <deekshith@systemsvalley.co.in>
2020-09-23 10:53:02 +0530

    T0470-M10 private car

--- magnus_hr/models/hr.py
+++ magnus_hr/models/hr.py
@@ -25,6 +25,8 @@ class Employee(models.Model):
     employee_numbersid = fields.Char('Employee NMBRs ID')
     date_last_promotion = fields.Date('Date of last Promotion')
 
+    has_private_car = fields.Boolean(string="Employee has a private car")
+
     def validate_dates(self):
         start_date = self.official_date_of_employment
         end_date = self.end_date_of_employment
