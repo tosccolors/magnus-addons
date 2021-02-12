2da9fa08103d8496998904664b36da267519989a
hayo-1996 <h.bos@magnus.nl>
2021-02-12 14:57:04 +0100

    -Upgrade of Magnus Equipment Module -magnus_assets_equipment_link module which adds a field to assets, and which makes the field asset_ids visible on the equipment object.

---
+++ magnus_assets_equipment_link/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Magnus customization to the asset equipment link",
+    "summary": "Create equipment when validating an invoice with assets",
+    "version": "10.0.1.0.0",
+    "website": "http://www.magnus.nl",
+    "author": "Hayo Bos",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account_asset_maintenance",
+    ],
+    "data": [
+        "views/account_asset_asset_views.xml",
+        "maintenance_equipment_view.xml"
+    ],
+}
--- magnus_equipment/__manifest__.py
+++ magnus_equipment/__manifest__.py
@@ -3,18 +3,19 @@
 {
     'name': 'Magnus - Equipments addon',
     'version': '1.0',
-    'author'  : 'Magnus - Willem Hulshof',
+    'author'  : 'Magnus - Hayo Bos',
     'website' : 'http://www.magnus.nl',
     'category': 'Maintenance',
-    'description': """Restricting Menu Visibility""",
-    'depends': ['maintenance','data_time_tracker'],
+    'description': """Adds Magnus specific requirements to the existing Equipment Module.""",
+    'depends': ['maintenance', 'data_time_tracker', 'account_asset_maintenance'],
     'summary': 'Equipments, Assets, Internal Hardware, Allocation Tracking',
     'data': [
         'security/security.xml',
+        'security/ir.model.access.csv',
         'views/maintenance_view.xml',
-        'views/maintenance_equipment_view.xml'
+        'views/maintenance_equipment_view.xml',
+        'data/maintenance_data.xml'
     ],
-
     'installable': True,
     'application': True,
 }
