92ea13fe079292df176a750ada3a6f77f51af881
kumarinie <kuamrisushmanie@gmail.com>
2021-03-25 18:58:17 +0530

    Partial commit for equipment button domain test Module: magnus_asset_management_analytic_account_quickfix

--- magnus_asset_management_analytic_account_quickfix/models/account_asset.py
+++ magnus_asset_management_analytic_account_quickfix/models/account_asset.py
@@ -19,3 +19,25 @@ class AccountAsset(models.Model):
             analytic_account = self.env['account.analytic.account'].browse(vals["account_analytic_id"])
             vals['analytic_account_id_2'] = analytic_account.id
         return super(AccountAsset, self).create(vals)
+
+    #override the button action to test domain bug
+    @api.multi
+    def button_open_equipment(self):
+        self.ensure_one()
+        res = self.env.ref('maintenance.hr_equipment_action').read()[0]
+        res['domain'] = [('asset_ids', 'in', self.ids)]
+        res['context'] = {'default_asset_ids': [(6, 0, self.ids)]}
+        return res
+
+
+class AccountAssetAsset(models.Model):
+    _inherit = 'account.asset.asset'
+
+    # override the button action to test domain bug
+    @api.multi
+    def button_open_equipment(self):
+        self.ensure_one()
+        res = self.env.ref('maintenance.hr_equipment_action').read()[0]
+        res['domain'] = [('asset_ids', 'in', self.ids)]
+        res['context'] = {'default_asset_ids': [(6, 0, self.ids)]}
+        return res
