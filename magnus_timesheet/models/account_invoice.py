d5aca253c9379900ab4c3d639154526d01199117
Willem Hulshof <w.hulshof@magnus.nl>
2022-03-07 13:41:49 +0100

    foutje

--- magnus_timesheet/models/account_invoice.py
+++ magnus_timesheet/models/account_invoice.py
@@ -120,7 +120,7 @@ class AccountInvoice(models.Model):
         if to_process_invoices:
             to_process_invoices.action_create_ic_lines()
         if supplier_invoices:
-            supplier_invoices.fill_trading_partner_code()
+            supplier_invoices.fill_trading_partner_code_supplier_invoice()
         res = super(AccountInvoice, self).action_invoice_open()
         for invoice in to_process_invoices:
             analytic_invoice_id = invoice.invoice_line_ids.mapped('analytic_invoice_id')
