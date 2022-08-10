4ebd5bcee1291d98cc519b0fdec99d39d80ec200
kumarinie <kuamrisushmanie@gmail.com>
2022-08-10 12:47:44 +0530

    Merge magnus_co_manager

--- magnus_co_manager/models/hr_timesheet_sheet.py
+++ magnus_co_manager/models/hr_timesheet_sheet.py
@@ -10,12 +10,14 @@ class HrTimesheetSheet(models.Model):
         users = super(HrTimesheetSheet, self)._get_validator_user_ids()
         for timesheet in self:
             managers_to_add = []
-            comanagers_ids_dept = [manager.id for manager in timesheet.department_id.manager_2_ids]
-            comanagers_ids_parent_dept = [manager.id for manager in timesheet.department_id.parent_id.manager_2_ids]
+            comanagers_ids_dept = [manager.user_id.id for manager in timesheet.department_id.manager_2_ids]
+            comanagers_ids_parent_dept = [manager.user_id.id for manager in timesheet.department_id.parent_id.manager_2_ids]
             if comanagers_ids_dept and self.env.uid not in comanagers_ids_dept:
                 managers_to_add = managers_to_add + comanagers_ids_dept
             elif comanagers_ids_parent_dept and self.env.uid not in comanagers_ids_parent_dept:
                 managers_to_add = managers_to_add + comanagers_ids_parent_dept
+
+            managers_to_add = list(set(managers_to_add))
             for manager in managers_to_add:
-                users.append(manager)
+                if bool(manager): users.append(manager)
         return users
