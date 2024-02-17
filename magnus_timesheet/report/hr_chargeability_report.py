32769e0c5bb86608cc1b71e3a9450c6157f88f0e
Willem Hulshof <whulshof@TOSC.NL>
2024-02-17 20:32:07 +0100

    another fix

--- magnus_timesheet/report/hr_chargeability_report.py
+++ magnus_timesheet/report/hr_chargeability_report.py
@@ -64,73 +64,92 @@ class HrChargeabilityReport(models.Model):
         tools.drop_view_if_exists(self.env.cr, 'hr_chargeability_report')
         self.env.cr.execute("""
             CREATE OR REPLACE VIEW hr_chargeability_report AS (
-            SELECT id,
-				date,
-				user_id,
-				operating_unit_id,
-				department_id,
-				external,
-				ts_optional,
-				ts_no_8_hours_day,
-				captured_hours,
-				chargeable_hours,
-				norm_hours,
-				chargeability
-            FROM
-                (SELECT
-                    min(aa.id) as id,
-                    aa.date as date,
-                    aa.user_id as user_id,
-                    aa.operating_unit_id as operating_unit_id,
-                    aa.department_id as department_id,
-                    emp.external as external,
-                    emp.timesheet_optional as ts_optional,
-                    emp.timesheet_no_8_hours_day as ts_no_8_hours_day,
-                    SUM(unit_amount) as captured_hours,
-                    SUM(CASE 
-                             WHEN aa.chargeable = 'true' 
-                             THEN unit_amount 
-                             ELSE 0 
-                        END) as chargeable_hours,
-                    (COUNT (DISTINCT aa.date) * (
+            with sq as (
+			SELECT
+						min(aa.id) as id,
+						aa.date as date,				
+						aa.user_id as user_id,
+						aa.operating_unit_id as operating_unit_id,
+						aa.department_id as department_id,
+						emp.external as external,
+						emp.timesheet_optional as ts_optional,
+						emp.timesheet_no_8_hours_day as ts_no_8_hours_day,
+						SUM(unit_amount) as captured_hours,
+						SUM(CASE 
+										 WHEN aa.chargeable = 'true' 
+										 THEN unit_amount 
+										 ELSE 0 
+								END) as chargeable_hours
+			FROM account_analytic_line aa
+						JOIN resource_resource resource 
+						ON (resource.user_id = aa.user_id)
+						JOIN hr_employee emp 
+						ON (emp.resource_id = resource.id)
+			WHERE aa.product_uom_id = 5
+								AND aa.planned = FALSE
+								AND (aa.ot = FALSE or aa.ot is null)
+								AND aa.project_id IS NOT NULL 
+								AND resource.active = TRUE
+								AND aa.state != 'change-chargecode'							
+			GROUP BY 
+								aa.operating_unit_id, 
+								aa.user_id,  
+								aa.date, 
+								aa.department_id, 
+								aa.state,
+								emp.external, 
+								emp.timesheet_optional, 
+								emp.timesheet_no_8_hours_day
+			ORDER BY  aa.date),
+						sq2 as (
+			SELECT 
+						aa2.date,				
+						aa2.user_id,
+						SUM(aa2.unit_amount) as captured_hours,
+						(COUNT (DISTINCT aa2.date) * (
 							 CASE 
-                             WHEN dr.date_end - aa.date > 1
-                             THEN 8 
-                             ELSE 0 
+										 WHEN dr2.date_end - aa2.date > 1
+										 THEN 8 
+										 ELSE 0 
 							 END
 							 )
-                    - SUM(
-                        CASE 
-                            WHEN aa.correction_charge = 'true' 
-                            THEN unit_amount 
-                            ELSE 0 
-                        END)) as norm_hours,
-                    0.0  as chargeability
-                FROM account_analytic_line aa
-                JOIN resource_resource resource 
-                ON (resource.user_id = aa.user_id)
-                JOIN hr_employee emp 
-                ON (emp.resource_id = resource.id)
-                JOIN date_range dr 
-                ON (dr.id = aa.week_id)
-                WHERE aa.product_uom_id = %s 
-                    AND aa.planned = FALSE
-                    AND (aa.ot = FALSE or aa.ot is null)
-                    AND aa.project_id IS NOT NULL 
-                    AND resource.active = TRUE
-                    AND aa.state != 'change-chargecode'
-                GROUP BY 
-                    aa.operating_unit_id, 
-                    aa.user_id, 
-                    dr.date_end, 
-                    aa.date, 
-                    aa.department_id, 
-                    emp.external, 
-                    emp.timesheet_optional, 
-                    emp.timesheet_no_8_hours_day
-				ORDER BY aa.date) as selection
-				WHERE selection.captured_hours > 0
-            )""" % (uom))
+                         - SUM(
+								CASE 
+										WHEN aa2.correction_charge = 'true' 
+										THEN aa2.unit_amount 
+										ELSE 0 
+								END)) as norm_hours
+					from account_analytic_line aa2 
+					JOIN resource_resource resource2 
+						ON (resource2.user_id = aa2.user_id)
+					JOIN date_range dr2 
+						ON (dr2.id = aa2.week_id)
+					WHERE aa2.product_uom_id = 5
+								AND aa2.planned = FALSE
+								AND (aa2.ot = FALSE or aa2.ot is null)
+								AND aa2.project_id IS NOT NULL 
+								AND resource2.active = TRUE
+								AND aa2.state != 'change-chargecode'			
+					GROUP BY aa2.user_id, 
+								aa2.date,
+								dr2.date_end
+					)
+					SELECT 
+								sq.id,
+								sq.date, 
+								sq.user_id, 
+								sq.operating_unit_id, 
+								sq.department_id, 
+								sq.external,
+								sq.ts_optional,
+								sq.ts_no_8_hours_day,
+								sq.captured_hours, 
+								sq.chargeable_hours,
+								sq.captured_hours / sq2.captured_hours * sq2.norm_hours as norm_hours,
+								0.0  as chargeability
+					FROM sq, sq2
+					WHERE sq2.date = sq.date
+		    	)""" % (uom))
 
 
     @api.model
