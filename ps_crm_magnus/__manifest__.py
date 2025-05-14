cefe999305499f3b38e07756f1e7fa6c4ae30c74
Holger Brunn <mail@hunki-enterprises.com>
2025-05-14 10:25:46 +0200

    [ADD] #792 ps_crm_magnus

---
+++ ps_crm_magnus/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2025 The Open Source Company (www.tosc.nl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Professional Services CRM (Magnus)",
+    "summary": "Magnus customizations for CRM",
+    "license": "AGPL-3",
+    "author": "The Open Source Company",
+    "website": "http://www.tosc.nl",
+    "category": "Sales",
+    "version": "14.0.1.0.0",
+    "depends": [
+        "ps_crm",
+        "sale_crm",
+    ],
+    "data": [
+        "security/ps_crm_magnus.xml",
+        "views/crm_lead.xml",
+        "views/menu.xml",
+    ],
+    "demo": [
+    ],
+}
